80e03539bda0e06427a66b9cf303fb4f055fdfec
彭帮政 <v_BangzhengPeng@budweiserapac.com>
2021-02-01 16:35:30 +0800

    原型模式（原型设计模式）详解

---
+++ src/main/java/com/sise/原型模式/PrototypeTest.java
@@ -0,0 +1,28 @@
+package com.sise.原型模式;
+
+
+/**
+ * 具体原型类
+ */
+class Realizetype implements Cloneable {
+    Realizetype() {
+        System.out.println("具体原型创建成功！");
+    }
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        System.out.println("具体原型复制成功！");
+        return (Realizetype) super.clone();
+    }
+}
+
+/**
+ * 原型模式的测试类
+ * @author lenm
+ */
+public class PrototypeTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Realizetype obj1 = new Realizetype();
+        Realizetype obj2 = (Realizetype) obj1.clone();
+        System.out.println("obj1==obj2?" + (obj1 == obj2));
+    }
+}
